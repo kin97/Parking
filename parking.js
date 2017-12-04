6e0214ed6abdc2555851c71579f41f5f0114a073
kin97 <oscarcasal1997@gmail.com>
2017-12-04 20:21:50 +0100

    Funcion MeterCoche ok

---
+++ parking.js
@@ -0,0 +1,59 @@
+"use strict"
+/**
+ * @fileOverview Aplicacion de Gestion de Parking
+ * @author Oscar Casal
+ * @version 1.0
+ */
+/**
+ * @class Parking
+ * @property {number} capacidad Numero de coches que puede haber en el parking
+ * @prop {Array} TunelLavado Lista de coches para lavar
+ * @property {Array} coches Lista de coches en el parking
+ */
+class Parking {
+    /**
+     * 
+     * @param {Number} capacidad Numero de coches que puede haber en el parking
+     */
+    constructor(capacidad) {
+        this.capacidad = capacidad;
+        this.tunelLavado = [];
+        this.coches = [];
+    }
+    /**
+     * @description funcion que añade coch a nuestro garaje
+     * @param {Object} objeto coche que meteremos en el parking
+     * @returns {Number} -1, parking lleno, 0 ok  
+     */
+
+    MeterCoche(objeto) {
+        if (this.capacidad > 0) {
+            this.capacidad--;
+            if (objeto.lavado == true) {
+                this.tunelLavado.push(objeto.matricula)
+            } else {
+                this.coches.push(objeto.matricula)
+            }
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+}
+let parking = new Parking(2);
+let coche1={
+    matricula:"G321234",
+    lavado: true
+}
+let coche2={
+    matricula:"G321235",
+    lavado: false}
+let coche3={
+    matricula:"G321236",
+    lavado: false
+}
+console.log(parking.MeterCoche(coche1))
+console.log(parking.MeterCoche(coche2))
+console.log(parking.MeterCoche(coche1))
+console.log(parking)
